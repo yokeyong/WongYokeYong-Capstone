43b45854da9eef562ff30d5a965c0e66bc8d6a94
yokeyong <yokeyong.wong@gmail.com>
2025-03-02 14:30:19 +0800

    capstone-part-2

--- stockform/src/FinanceDashboard.jsx
+++ stockform/src/FinanceDashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import StockList from "./StocksList";
 
 function FinanceForm() {
@@ -8,6 +8,27 @@ function FinanceForm() {
     const formattedYestDate = yestDate.toISOString().split('T')[0];
     
     const [apiResponse, setApiResponse] = useState({})
+    const [formData, setFormData] = useState({})
+
+    useEffect(() => {
+        if (formData) {
+            const {symbol, quantity, priceInput} = formData;
+            // 1st account 88XJ721RN2667TQT
+            // 2nd account C7LJKZ5EGOBJ9HF9
+            fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=C7LJKZ5EGOBJ9HF9`)
+                .then((response) => response.status === 200 ? response.json() : console.log("Error"))
+                .then((data) => {
+                    setApiResponse({
+                        symbol: symbol,
+                        latestPrice: data["Time Series (Daily)"][formattedYestDate]["4. close"],
+                        quantity: quantity,
+                        priceInput: priceInput
+                    })
+                }
+            )
+        }
+    }
+    , [formData, formattedYestDate])
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -16,22 +37,17 @@ function FinanceForm() {
         const quantity = form.quantity.value
         const price = form.price.value
 
-        fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=88XJ721RN2667TQT`)
-            .then((response) => response.status === 200 ? response.json() : console.log("Error"))
-            .then((data) => {
-                setApiResponse({
-                    symbol: symbol,
-                    latestPrice: data["Time Series (Daily)"][formattedYestDate]["4. close"],
-                    quantity: quantity,
-                    priceInput: price
-                })
-            })
+        setFormData({
+            symbol: symbol,
+            quantity: quantity,
+            priceInput: price
+        })
     }
 
     return (
         <div>
-            <h3> Finance Dashboard </h3>
             <div className="FinanceForm" onSubmit={handleSubmit}>
+                <h3> Finance Dashboard </h3>
                 <form>
                     <label>
                     <input type="text" name="symbol" placeholder="Stock Symbol" />
--- stockform/src/StocksList.jsx
+++ stockform/src/StocksList.jsx
@@ -1,16 +1,38 @@
-function StockList(props) {
+import { useEffect, useState } from "react";
 
-    const difference = (props.stockInformation.latestPrice - props.stockInformation.priceInput) * props.stockInformation.quantity
+function StockList(props) {
+    const [stockList, setStockList] = useState([]);
+    let difference = 0;
+
+    useEffect(() => {
+        if (props.stockInformation.symbol !== "None") {
+            const difference = (props.stockInformation.latestPrice - props.stockInformation.priceInput) * props.stockInformation.quantity;
+            setStockList([
+                <div className="StockList" key="StockList">
+                    <h3>Stocks List</h3>
+                    <li className="subList" key="subList">
+                        <ul className="Symbol" key="Symbol">Symbol: {props.stockInformation.symbol}</ul>
+                        <ul className="Quantity" key="Quantity">Quantity: {props.stockInformation.quantity}</ul>
+                        <ul className="PurchasePrice" key="PurchasePrice">Purchase Price: {props.stockInformation.priceInput}</ul>
+                        <ul className="ProfitLoss" key="ProfitLoss">Profit/Loss: {difference}</ul>
+                    </li>
+                </div>
+            ]);
+        } else {
+            setStockList([
+                <div className="StockList">
+                    <h3>Stocks List</h3>
+                    <p className="noStock">No stocks are available.</p>
+                </div>
+            ]);
+        }
+    }, [props]);
 
     return (
-        <div className="StockList">
-            <h3>Stocks List</h3>
-            <p id="Symbol">Symbol: {props.stockInformation.symbol}</p>
-            <p id="Quantity">Quantity: {props.stockInformation.quantity}</p>
-            <p id="PurchasePrice">Purchase Price: {props.stockInformation.priceInput}</p>
-            <p id="ProfitLoss">Profit/Loss: {difference}</p>
+        <div>
+            {stockList}
         </div>
-    )
+    );
 }
 
 export default StockList;
